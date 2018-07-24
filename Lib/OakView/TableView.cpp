e5f8ccd2ea985f236110f896a9385acf6c4adf4e
Mikkel Nøhr Løvgreen <m_loevgreen@hotmail.com>
2018-07-24 07:36:25 +0200

    First version of TableView

--- Lib/OakView/TableView.cpp
+++ Lib/OakView/TableView.cpp
@@ -6,7 +6,103 @@ namespace View {
 
 // =============================================================================
 // (public)
-TableView::TableView()
+TableView::TableView(QWidget *parent)
+    : QTableWidget(parent)
+{
+
+}
+
+// =============================================================================
+// (public)
+TableView::~TableView()
+{
+
+}
+
+// =============================================================================
+// (public)
+void TableView::setOakModel(Model::OakModel *model)
+{
+    if (m_model == model) { return; }
+
+    if (m_model) {
+        // Disconnect the old model
+//        m_model->notifier_currentItemChanged.remove(this);
+//        m_model->notifier_rootNodeDefChanged.remove(this);
+//        m_model->notifier_rootNodeChanged.remove(this);
+//        m_model->notifier_destroyed.remove(this);
+
+        m_model->notifier_itemInserted.remove(this);
+        m_model->notifier_itemMoved.remove(this);
+        m_model->notifier_itemCloned.remove(this);
+        m_model->notifier_itemRemoved.remove(this);
+
+        m_model->notifier_itemValueChanged.remove(this);
+    }
+
+    // Change the model
+    m_model = model;
+
+    if (m_model) {
+        // connect the new mobel
+//        m_model->notifier_currentItemChanged.add(this, &TableView::currentItemChanged);
+//        m_model->notifier_rootNodeDefChanged.add(this, &ListView::updateTreeStructure);
+//        m_model->notifier_rootNodeChanged.add(this, &ListView::updateTreeStructure);
+//        m_model->notifier_destroyed.add(this, &ListView::modelDestroyed);
+
+        m_model->notifier_itemInserted.add(this, &TableView::onItemInserted);
+        m_model->notifier_itemMoved.add(this, &TableView::onItemMoved);
+        m_model->notifier_itemCloned.add(this, &TableView::onItemCloned);
+        m_model->notifier_itemRemoved.add(this, &TableView::onItemRemoved);
+
+        m_model->notifier_itemValueChanged.add(this, &TableView::onItemValueChanged);
+    }
+}
+
+// =============================================================================
+// (public)
+void TableView::setRootItem(const Model::Item &item)
+{
+    if (!m_rootItem.isNull()) {
+        // Do nothing if the root item is the same
+        if (m_rootItem == item) { return; }
+    }
+    m_rootItem = item;
+
+
+}
+
+// =============================================================================
+// (public)
+void TableView::onItemInserted(const Model::Item &parentItem, int index)
+{
+
+}
+
+// =============================================================================
+// (public)
+void TableView::onItemMoved(const Model::Item &sourceParentItem, int sourceIndex, const Model::Item &targetParentItem, int targetIndex)
+{
+
+}
+
+// =============================================================================
+// (public)
+void TableView::onItemCloned(const Model::Item &sourceParentItem, int sourceIndex, const Model::Item &targetParentItem, int targetIndex)
+{
+
+}
+
+// =============================================================================
+// (public)
+void TableView::onItemRemoved(const Model::Item &parentItem, int index)
+{
+
+}
+
+// =============================================================================
+// (public)
+void TableView::onItemValueChanged(const Model::Item &item, int valueIndex)
 {
 
 }
--- Lib/OakView/TableView.h
+++ Lib/OakView/TableView.h
@@ -2,6 +2,8 @@
 
 #include <QTableWidget>
 
+#include "OakModel.h"
+
 namespace Oak {
 namespace View {
 
@@ -11,7 +13,21 @@ namespace View {
 class TableView : public QTableWidget
 {
 public:
-    TableView();
+    TableView(QWidget *parent = nullptr);
+    virtual ~TableView() override;
+
+    void setOakModel(Model::OakModel* model);
+    void setRootItem(const Model::Item& item);
+
+    void onItemInserted(const Model::Item& parentItem, int index);
+    void onItemMoved(const Model::Item& sourceParentItem, int sourceIndex, const Model::Item& targetParentItem, int targetIndex);
+    void onItemCloned(const Model::Item& sourceParentItem, int sourceIndex, const Model::Item& targetParentItem, int targetIndex);
+    void onItemRemoved(const Model::Item& parentItem, int index);
+    void onItemValueChanged(const Model::Item &item, int valueIndex);
+
+protected:
+    Model::OakModel * m_model = nullptr;
+    Model::Item m_rootItem;
 };
 
 } // namespace View
