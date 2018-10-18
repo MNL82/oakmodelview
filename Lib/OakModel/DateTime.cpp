e2afaf554ba2e03d6d411a9c5908a80c6fb89de2
Mikkel Nøhr Løvgreen <m_loevgreen@hotmail.com>
2018-10-18 09:02:52 +0200

    Version 1.0 of the DateTime class

--- Lib/OakModel/DateTime.cpp
+++ Lib/OakModel/DateTime.cpp
@@ -15,9 +15,11 @@
 #include <iomanip>
 #include <ctime>
 #include <chrono>
+#include <vector>
 
-#define __STDC_WANT_LIB_EXT1__ 1
 #include <time.h>
+
+#include "../ServiceFunctions/Trace.h"
 
 namespace Oak {
 namespace Model {
@@ -25,59 +27,198 @@ namespace Model {
 // =============================================================================
 // (public)
 DateTime::DateTime()
+    : m_time { _emptyTimePoint() }
 {
-    m_time = std::chrono::system_clock::now();
+    //TRACE(L"Constructor");
 }
 
 // =============================================================================
 // (public)
-DateTime::DateTime(const std::string &str, const std::string & locale)
+DateTime::DateTime(int year, int month, int day, int hour, int minute, int secound, TimeZone timeZone)
+    : DateTime(toString(ProcessedDateTime{ year, month, day, hour, minute, secound }), timeZone, "%Y-%m-%d %H:%M:%S")
 {
+}
 
+// =============================================================================
+// (public)
+DateTime::DateTime(const std::string &inputStr, TimeZone timeZone, const char * format)
+{
+    //TRACE(L"Constructor(%S, %S, %S)", str.c_str(), timeZone == TimeZone::UTC ? "UTC" : "Local", format);
     std::tm tm = {};
-    std::istringstream ss(str);
-    ss.imbue(std::locale(locale));
-    ss >> std::get_time(&tm, "%Y %m %d  %H:%M:%S");
-    m_time = std::chrono::system_clock::from_time_t(std::mktime(&tm));
+    std::istringstream iss(inputStr);
+    iss >> std::get_time(&tm, format);
+
+    if (iss.fail()) {
+        TRACE(L"Faild to construct DateTime from string %S (%S)", inputStr.c_str(), format);
+        return;
+    }
+    std::time_t tt = std::mktime(&tm);
+    m_time = std::chrono::system_clock::from_time_t(tt);
+
+    // Time is parsed without taking account for daylight savings flag
+    if (tm.tm_isdst) { m_time -= std::chrono::hours(1); }
+
+    if (timeZone == TimeZone::UTC) {
+        // Time is parsed as local time and needs to be adjusted to UTC time
+        std::tm tm_UTC;
+        gmtime_s(&tm_UTC, &tt);
+
+        int offset = tm.tm_hour - tm_UTC.tm_hour;
+
+        // Adjust for changing day/year
+        if (tm.tm_year != tm_UTC.tm_year) {
+            offset += tm.tm_year > tm_UTC.tm_year ? 24 : -24;
+        } else if (tm.tm_yday != tm_UTC.tm_yday) {
+            offset += tm.tm_yday > tm_UTC.tm_yday ? 24 : -24;
+        }
+
+        //TRACE(L"Local time zone offset: %i", offset);
+        m_time += std::chrono::hours(offset);
+    }
+
+    // End to end testing: toString() result should be equal to 'inputStr'
+    std::string outputStr = toString(timeZone, format);
+    if (inputStr != outputStr) {
+        TRACE(L"Testing faild: %S != %S", inputStr.c_str(), outputStr.c_str());
+        m_time = _emptyTimePoint();
+        ASSERT(false);
+    }
+    // TESTING END
 }
 
 // =============================================================================
 // (public)
-int DateTime::year() const
+DateTime::DateTime(const DateTime &copy) noexcept
+    : m_time { copy.m_time }
+{
+    //TRACE(L"Copy constructor");
+}
+
+// =============================================================================
+// (public)
+DateTime::DateTime(DateTime &&move) noexcept
+    : m_time { std::move(move.m_time) }
+{
+    //TRACE(L"Move constructor");
+}
+
+// =============================================================================
+// (public)
+DateTime &DateTime::operator=(const DateTime &copy)
+{
+    //TRACE(L"Copy assignment");
+    m_time = copy.m_time;
+    return *this;
+}
+
+// =============================================================================
+// (public)
+DateTime &DateTime::operator=(DateTime &&move)
+{
+    //TRACE(L"Move assignment");
+    m_time = std::move(move.m_time);
+    return *this;
+}
+
+// =============================================================================
+// (public)
+bool DateTime::operator==(const DateTime &other) const
+{
+    return m_time == other.m_time;
+}
+
+// =============================================================================
+// (public)
+bool DateTime::operator!=(const DateTime &other) const
+{
+    return m_time != other.m_time;
+}
+
+// =============================================================================
+// (public)
+bool DateTime::operator<(const DateTime &other) const
+{
+    return m_time < other.m_time;
+}
+
+// =============================================================================
+// (public)
+bool DateTime::isNull() const
+{
+    return m_time == _emptyTimePoint();
+}
+
+// =============================================================================
+// (public)
+DateTime DateTime::now()
+{
+    DateTime dt;
+    dt.m_time = std::chrono::system_clock::now();
+    return dt;
+}
+
+// =============================================================================
+// (public)
+void DateTime::swap(DateTime &other) noexcept
+{
+    //TRACE(L"swap()");
+    std::swap(m_time, other.m_time);
+}
+
+// =============================================================================
+// (public)
+int DateTime::year(DateTime::TimeZone timeZone) const
 {
     time_t tt = std::chrono::system_clock::to_time_t(m_time);
     tm tm;
-    gmtime_s(&tm, &tt);
+    if (timeZone == TimeZone::UTC) {
+        gmtime_s(&tm, &tt);
+    } else if (timeZone == TimeZone::Local) {
+        localtime_s(&tm, &tt);
+    }
     return 1900 +  tm.tm_year;
 }
 
 // =============================================================================
 // (public)
-int DateTime::month() const
+int DateTime::month(TimeZone timeZone) const
 {
     time_t tt = std::chrono::system_clock::to_time_t(m_time);
     tm tm;
-    gmtime_s(&tm, &tt);
+    if (timeZone == TimeZone::UTC) {
+        gmtime_s(&tm, &tt);
+    } else if (timeZone == TimeZone::Local) {
+        localtime_s(&tm, &tt);
+    }
     return 1 +  tm.tm_mon;
 }
 
 // =============================================================================
 // (public)
-int DateTime::day() const
+int DateTime::day(DateTime::TimeZone timeZone) const
 {
     time_t tt = std::chrono::system_clock::to_time_t(m_time);
     tm tm;
-    gmtime_s(&tm, &tt);
+    if (timeZone == TimeZone::UTC) {
+        gmtime_s(&tm, &tt);
+    } else if (timeZone == TimeZone::Local) {
+        localtime_s(&tm, &tt);
+    }
     return tm.tm_mday;
 }
 
 // =============================================================================
 // (public)
-int DateTime::hour() const
+int DateTime::hour(DateTime::TimeZone timeZone) const
 {
-    std::chrono::system_clock::duration tp = m_time.time_since_epoch();
-    tp -= std::chrono::duration_cast<chrone_days>(tp);
-    return std::chrono::duration_cast<std::chrono::hours>(tp).count();
+    time_t tt = std::chrono::system_clock::to_time_t(m_time);
+    tm tm;
+    if (timeZone == TimeZone::UTC) {
+        gmtime_s(&tm, &tt);
+    } else if (timeZone == TimeZone::Local) {
+        localtime_s(&tm, &tt);
+    }
+    return tm.tm_hour;
 }
 
 // =============================================================================
@@ -100,36 +241,49 @@ int DateTime::secound() const
 
 // =============================================================================
 // (public)
-ProcessedDateTime DateTime::dateTimeUTC() const
+ProcessedDateTime DateTime::toStruct(DateTime::TimeZone timeZone) const
 {
-    time_t tt = std::chrono::system_clock::to_time_t(m_time);
-    tm tm;
-    gmtime_s(&tm, &tt);
-    return _dateTime(tm);
+    std::time_t tt = std::chrono::system_clock::to_time_t(m_time);
+    std::tm tm;
+
+    if (timeZone == TimeZone::UTC) {
+        gmtime_s(&tm, &tt);
+    } else if (timeZone == TimeZone::Local) {
+        localtime_s(&tm, &tt);
+    }
+
+    ProcessedDateTime pdt;
+
+    pdt.year = 1900 + tm.tm_year;
+    pdt.month = 1 + tm.tm_mon;
+    pdt.day = tm.tm_mday;
+
+    pdt.hour = tm.tm_hour;
+    pdt.minute = tm.tm_min;
+    pdt.secound = tm.tm_sec;
+
+    return pdt;
 }
 
 // =============================================================================
 // (public)
-ProcessedDateTime DateTime::dateTimeLocal() const
+std::string DateTime::toString(TimeZone timeZone, const char *format) const
 {
-    time_t tt = std::chrono::system_clock::to_time_t(m_time);
-    tm tm;
-    localtime_s(&tm, &tt);
-    return _dateTime(tm);
-}
+    if (format == nullptr) {
+        return toString(toStruct(timeZone));
+    }
+    std::time_t tt = std::chrono::system_clock::to_time_t(m_time);
+    std::tm tm;
 
-// =============================================================================
-// (public)
-std::string DateTime::toStringLocal() const
-{
-    return toString(dateTimeLocal());
-}
+    if (timeZone == TimeZone::UTC) {
+        gmtime_s(&tm, &tt);
+    } else if (timeZone == TimeZone::Local) {
+        localtime_s(&tm, &tt);
+    }
 
-// =============================================================================
-// (public)
-std::string DateTime::toStringUTC() const
-{
-    return toString(dateTimeUTC());
+    std::ostringstream ss;
+    ss << std::put_time(&tm, format);
+    return ss.str();
 }
 
 // =============================================================================
@@ -164,34 +318,66 @@ std::string DateTime::toString(const ProcessedDateTime &pdt) const
 
 // =============================================================================
 // (public)
-std::string DateTime::toString(const char *format)
+const DateTime &DateTime::emptyDateTime()
 {
-    std::time_t tt = std::time(nullptr);
-    std::tm tm;
-    localtime_s(&tm, &tt);
-
-    std::ostringstream ss;
-    ss << std::put_time(&tm, format);
-    return ss.str();
+    static DateTime s_emptyDateTime = DateTime();
+    return s_emptyDateTime;
 }
 
 // =============================================================================
-// (protected)
-ProcessedDateTime DateTime::_dateTime(tm t) const
+// (public)
+// Return the number of days of the previous months
+int DateTime::_monthDaySum(size_t month)
 {
-    ProcessedDateTime pdt;
-
-    pdt.year = 1900 + t.tm_year;
-    pdt.month = 1 + t.tm_mon;
-    pdt.day = t.tm_mday;
-
-    pdt.hour = t.tm_hour;
-    pdt.minute = t.tm_min;
-    pdt.secound = t.tm_sec;
-
-    return pdt;
+    ASSERT(month >= 1 && month <= 12);
+    static std::vector<int> s_monthDaySum;
+    if (s_monthDaySum.empty()) {
+        s_monthDaySum.resize(13);
+        s_monthDaySum[1] = 0;
+        for (size_t i = 2; i < 13; i++) {
+            s_monthDaySum[i] = s_monthDaySum[i-1] + _monthDays(i-1);
+        }
+    }
+    return s_monthDaySum[month];
 }
 
+// =============================================================================
+// (public)
+int DateTime::_monthDays(size_t month)
+{
+    ASSERT(month >= 1 && month <= 12);
+    static std::vector<int> s_monthDays;
+    if (s_monthDays.empty()) {
+        s_monthDays.resize(13);
+        s_monthDays[1] = 31;
+        s_monthDays[2] = 28;
+        s_monthDays[3] = 31;
+        s_monthDays[4] = 30;
+        s_monthDays[5] = 31;
+        s_monthDays[6] = 30;
+        s_monthDays[7] = 31;
+        s_monthDays[8] = 31;
+        s_monthDays[9] = 30;
+        s_monthDays[10] = 31;
+        s_monthDays[11] = 30;
+        s_monthDays[12] = 31;
+    }
+    return s_monthDays[month];
+}
+
+// =============================================================================
+// (public)
+const std::chrono::system_clock::time_point &DateTime::_emptyTimePoint()
+{
+    static auto s_emptyTimePoint = std::chrono::system_clock::time_point(std::chrono::seconds(-1));
+    return s_emptyTimePoint;
+}
+
+// =============================================================================
+// (public)
+void swap(DateTime &left, DateTime &right) noexcept {
+    left.swap(right);
+}
 
 } // namespace Model
 } // namespace Oak
--- Lib/OakModel/DateTime.h
+++ Lib/OakModel/DateTime.h
@@ -23,9 +23,32 @@ struct ProcessedDateTime {
     int year;
     int month;
     int day;
+
     int hour;
     int minute;
     int secound;
+
+    ProcessedDateTime & operator-=(const ProcessedDateTime &other)
+    {
+        year -= other.year;
+        month -= other.month;
+        day -= other.day;
+
+        hour -= other.hour;
+        minute -= other.minute;
+        secound -= other.secound;
+        return *this;
+    }
+
+    bool isNull() const
+    {
+        return year == 0 &&
+               month == 0 &&
+               day == 0 &&
+               hour == 0 &&
+               minute == 0 &&
+               secound == 0;
+    }
 };
 
 // =============================================================================
@@ -34,33 +57,61 @@ struct ProcessedDateTime {
 class DateTime
 {
 public:
+
+    enum class TimeZone {
+        UTC = 0,
+        Local = 1
+    };
+
     DateTime();
-    DateTime(const std::string &str, const std::string &locale = "");
+    DateTime(int year, int month, int day, int hour = 0, int minute = 0, int secound = 0, TimeZone timeZone = TimeZone::Local);
+    DateTime(const std::string &str, TimeZone timeZone = TimeZone::Local, const char *format = "%Y-%m-%d %H:%M:%S");
 
-    int year() const;
-    int month() const;
-    int day() const;
+    DateTime(const DateTime &copy) noexcept;
+    DateTime(DateTime &&move) noexcept;
 
-    int hour() const;
+    DateTime &operator=(const DateTime &copy);
+    DateTime &operator=(DateTime &&move);
+
+    bool operator==(const DateTime &other) const;
+    bool operator!=(const DateTime &other) const;
+
+    bool operator<(const DateTime &other) const;
+
+    bool isNull() const;
+
+    static DateTime now();
+
+    void swap(DateTime &other) noexcept;
+
+    int year(DateTime::TimeZone timeZone = TimeZone::Local) const;
+    int month(DateTime::TimeZone timeZone = TimeZone::Local) const;
+    int day(TimeZone timeZone = TimeZone::Local) const;
+
+    int hour(DateTime::TimeZone timeZone = TimeZone::Local) const;
     int minute() const;
     int secound() const;
 
-    ProcessedDateTime dateTimeUTC() const;
-    ProcessedDateTime dateTimeLocal() const;
+    ProcessedDateTime toStruct(TimeZone timeZone = TimeZone::Local) const;
 
-    std::string toStringLocal() const;
-    std::string toStringUTC() const;
+    std::string toString(TimeZone timeZone = TimeZone::Local, const char *format = "%Y-%m-%d %H:%M:%S") const;
     std::string toString(const ProcessedDateTime & pdt) const;
 
-    std::string toString(const char *format = "%Y-%m-%d %H:%M:%S");
+    static const DateTime & emptyDateTime();
+
 protected:
-    ProcessedDateTime _dateTime(tm t) const;
+    static int _monthDaySum(size_t month);
+    static int _monthDays(size_t month);
+
+    static const std::chrono::system_clock::time_point & _emptyTimePoint();
 
 protected:
     std::chrono::system_clock::time_point m_time;
 };
 
-typedef std::chrono::duration<int, std::ratio_multiply<std::chrono::hours::period, std::ratio<24> >::type> chrone_days;
+void swap(Oak::Model::DateTime &left, Oak::Model::DateTime &right) noexcept;
+
+typedef std::chrono::duration<int, std::ratio_multiply<std::chrono::hours::period, std::ratio<24> >::type> chrono_days;
 
 } // namespace Model
 } // namespace Oak
